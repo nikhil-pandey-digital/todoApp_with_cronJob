c675ca0240ef0d5bf24ff343276a44d8be357f02
nikhil pandey <thenikhilpandey11@gmail.com>
2024-03-25 00:34:14 +0530

    cron jobs implemented

--- cronJobs.js
+++ cronJobs.js
@@ -2,19 +2,13 @@ const cron = require("node-cron");
 const twilio = require("twilio");
 const Task = require("./models/taskModel");
 const User = require("./models/userModel");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 
-const accountSid = "AC844f8c97a534f61238fd47b3a8f44fc1";
-const authToken = "25fd8d4a951f9b892dbc4f852b549f9e";
-const client = twilio(accountSid, authToken);
-
-// cron.schedule("* * * * *", async () => {
-//     client.calls.create({
-//         url: "http://demo.twilio.com/docs/voice.xml",
-//         to: "+919311056457",
-//         from: "+16562194062",
-//       })
-//       .then(call => console.log(call.sid));
-// });
+const client = twilio(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
 
 // This cron job will run every minute, and update the priority of each task based on its due date
 cron.schedule("* * * * *", async () => {
@@ -52,31 +46,28 @@ cron.schedule("* * * * *", async () => {
 
 //cron job for voice call
 cron.schedule("* * * * *", async () => {
-
-try {
+  try {
     const now = new Date();
-  const tasks = await Task.find({ due_date: { $lt: now } ,deleted_at:null})
-    .populate("assignee")
-    .exec();
+    const tasks = await Task.find({ due_date: { $lt: now }, deleted_at: null })
+      .populate("assignee")
+      .exec();
 
     console.log(tasks);
 
     for (const task of tasks) {
-        task.assignee.sort((a, b) => Number(a.priority) - Number(b.priority));
-       
-        for (const user of task.assignee) {
-          const call = await client.calls.create({
-            url: 'http://demo.twilio.com/docs/voice.xml',
-            to: `+91${user.phone_number}`,
-            from: '+16562194062',
-          });
-           console.log(call.sid);
-        //   const finalStatus = await waitForCallToComplete(call.sid);
-          if (call.status === 'completed') break;
-        }
+      task.assignee.sort((a, b) => Number(a.priority) - Number(b.priority));
+
+      for (const user of task.assignee) {
+        const call = await client.calls.create({
+          url: "http://demo.twilio.com/docs/voice.xml",
+          to: `+91${user.phone_number}`,
+          from: "+16562194062",
+        });
+        console.log(call.sid);
+        if (call.status === "completed") break;
       }
-} catch (error) {
+    }
+  } catch (error) {
     console.log(error);
-}
-  
-})
+  }
+});
